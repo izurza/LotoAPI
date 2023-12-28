8c9cd55af316eb2b7444dcec96c61dc6eeff213a
i.izurza <i.izurza@consulpyme.com>
2023-12-28 13:04:21 +0100

    gitignored

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import Navidad.NavidadRoutes as NavidadRoutes
-app = FastAPI()
-
-@app.get('/')
-def index():
-    return {'message':"LotoAPI V1"}
-
-app.include_router(NavidadRoutes.router)
